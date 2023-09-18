8dfa113bd40adfaf28e65ba6f3976e45df1921c5
Sashka <korotkova.aa2005@gmail.com>
2023-09-18 20:29:03 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> numbers = { 4, 7, 9, 14, 12 };
+
+	[&]() {
+		cout << "input data: ";
+		for (auto &i : numbers)
+		{
+			cout << i << " ";
+			if (i % 2 == 1) i *= 3;
+		}
+
+		cout << "\ninput data: ";
+		for (auto i : numbers)
+		{
+			cout << i << " ";
+		}
+	}();
+}
